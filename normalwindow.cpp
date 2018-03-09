e912ae9e9c328be1eda0d3a671c9e22cbc7bbf5e
Panda <16gbchen@stu.edu.cn>
2018-03-09 20:13:00 +0800

    change normalwindow

--- normalwindow.cpp
+++ normalwindow.cpp
@@ -1,14 +1,28 @@
 #include "normalwindow.h"
 #include "ui_normalwindow.h"
+//#include <QMessageBox>
+#include <QDialog>
+
 
 NormalWindow::NormalWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::NormalWindowUi)
 {
     ui->setupUi(this);
+    connect(ui->pushButton_timer,&QPushButton::clicked,this,&NormalWindow::clickButton);
 }
 
 NormalWindow::~NormalWindow()
 {
-
+    delete ui;
 }
+
+void NormalWindow::clickButton(){
+   //赛程的点击事件
+   //QMessageBox::about(this,tr("提示信息"),tr("更新成功"));
+
+    QDialog dialog;
+    dialog.setModal(true);
+    dialog.exec();
+}
+
